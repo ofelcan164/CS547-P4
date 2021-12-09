9831478928bde69def09af662a26cf50d8a461da
Oscar Felcan <ofelcan164@gmail.com>
2021-12-09 16:15:15 -0600

    Types.h updated

---
+++ types.h
@@ -0,0 +1,25 @@
+#include "udp.h"
+#include "mfs.h"
+
+#define BUFFER_SIZE (1000)
+#define REQ_SIZE (sizeof(struct request))
+#define RESP_SIZE (sizeof(struct response))
+
+enum msg_type {INIT, LOOKUP, STAT, WRITE, READ, CREAT, UNLINK, SHUTDOWN};
+
+struct request {
+    enum msg_type type;
+    char hostname[50];
+    int port;
+    int pinum;
+    char name[28];
+    int inum;
+    char buffer[BUFFER_SIZE];
+    int block;
+    int file_type;
+};
+
+struct response {
+    int rc;
+    MFS_Stat_t m;
+};
