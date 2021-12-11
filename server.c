e9f763d92420ca7132b0457844cff37f9d65eba6
Oscar Felcan <ofelcan164@gmail.com>
2021-12-11 16:10:38 -0600

    Initialization of FS image (with some TODOs)

--- server.c
+++ server.c
@@ -1,35 +1,39 @@
 #include <stdio.h>
 #include "udp.h"
 #include "types.h"
+#include "mfs.h"
 
-int lookup(int pinum, char* name) {
-
-}
-
-int stat(int inum) {
+int fs_lookup(int pinum, char* name) {
 
 }
 
-int write(int inum, char* buffer, int block) {
+int fs_stat(int inum) {
 
 }
 
-int read(int inum, int block) {
+int fs_write(int inum, char* buffer, int block) {
 
 }
 
-int create(int pinum, int type, char* name) {
+int fs_read(int inum, int block) {
 
 }
 
-int unlink(int pinum, char* name) {
+int fs_create(int pinum, int type, char* name) {
 
 }
 
-int shutdown() {
+int fs_unlink(int pinum, char* name) {
 
 }
 
+int fs_shutdown() {
+
+}
+
+
+int fd;
+struct checkpoint_region cr;
 // server code
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -37,6 +41,78 @@ int main(int argc, char *argv[]) {
     }
 
     // TODO: Initialize / Setup FS
+
+    // File system image file does not exist
+    if (access(argv[2], F_OK) == 0) {
+        fd = open(argv[2], O_RDWR); // Open image file
+
+        // Read checkpoint region
+        read(fd, (char *)&cr, sizeof(struct checkpoint_region));
+        fsync(fd);
+    } else {
+        fd = open(argv[2], O_RDWR | O_CREAT); // Open and create image file
+
+        // Create checkpoint region
+        cr.log_end_ptr = 0;
+        for (int i = 0; i < NUM_IMAP_PIECES; i++) {
+            cr.imap_pieces[i] = -1; // TODO et to -1 or init all pieces
+        }
+
+        // Add root directory
+        MFS_DirEnt_t root_entry_dot;
+        strcpy(root_entry_dot.name, ".");
+        root_entry_dot.inum = 0;
+        MFS_DirEnt_t root_entry_dot_dot;
+        strcpy(root_entry_dot_dot.name, "..");
+        root_entry_dot_dot.inum = 0;
+        
+        // Write . and .. entries to the data block and then the remaining empty entries
+        int root_data_ptr = lseek(fd, sizeof(cr), SEEK_SET); // Move file image offset TODO where to seek end of inode map or end of CR
+        write(fd, (char *)&root_entry_dot, sizeof(root_entry_dot));
+        write(fd, (char *)&root_entry_dot_dot, sizeof(root_entry_dot_dot));
+        for (int j = 0; int j < (MFS_BLOCK_SIZE - sizeof(root_entry_dot) - sizeof(root_entry_dot_dot)) / sizeof(MFS_DirEnt_t); j++) {
+            MFS_DirEnt_t new_ent;
+
+            new_ent.inum = -1;
+            write(fd, (char *)&new_ent, sizeof(MFS_DirEnt_t));
+        }
+
+        // Create root directory inode
+        struct inode root_dir;
+        root_dir.type = MFS_DIRECTORY;
+        for (int j = 0; j < NUM_POINTERS_PER_INODE; j++) {
+            if (j == 0) {
+                root_dir.pointers[j] = root_data_ptr;
+            }
+            else {
+                root_dir.pointers[j] = -1;
+            }
+        }
+        int root_dir_inode_ptr = lseek(fd, 0, SEEK_CUR);
+        // TODO SIZE IN INODE
+        write(fd, (char *)&root_dir, sizeof(root_dir));
+        
+        // Update the imap piece
+        struct imap_piece root_piece;
+        for (int j = 0; j < NUM_INODES_PER_PIECE; j++) {
+            if (j == 0) {
+                root_piece.inodes[j] = root_dir_inode_ptr;
+            }
+            else {
+                root_dir.pointers[j] = -1;
+            }
+        }
+        int root_imap_piece_ptr = lseek(fd, 0, SEEK_CUR);
+        write(fd, (char *)&root_piece, sizeof(root_piece)); // Write the imap piece
+
+        cr.imap_pieces[0] = root_imap_piece_ptr;
+        cr.log_end_ptr = lseek(fd, 0, SEEK_CUR);
+
+        lseek(fd, 0, SEEK_SET);
+        write(fd, (char *)&cr, sizeof(cr));
+        
+        fsync(fd); // Force to disk
+    }
 
     int sd = UDP_Open(atoi(argv[1]));
     assert (sd > -1);
--- types.h
+++ types.h
@@ -4,13 +4,17 @@
 #define BUFFER_SIZE (1000)
 #define REQ_SIZE (sizeof(struct request))
 #define RESP_SIZE (sizeof(struct response))
+#define NUM_IMAP_PIECES (256)
+#define FILENAME_SIZE (28)
+#define NUM_INODES_PER_PIECE (16)
+#define NUM_POINTERS_PER_INODE (14)
 
 enum req_type {LOOKUP, STAT, WRITE, READ, CREAT, UNLINK, SHUTDOWN};
 
 struct request {
     enum req_type type;
     int pinum;
-    char name[28];
+    char name[FILENAME_SIZE];
     int inum;
     char buffer[BUFFER_SIZE];
     int block;
@@ -23,4 +27,17 @@ struct response {
     char buffer[BUFFER_SIZE];
 };
 
-// Define response codes in enum?
+struct checkpoint_region {
+    int log_end_ptr;
+    int imap_pieces[NUM_IMAP_PIECES];
+}
+
+struct imap_piece {
+    int inodes[NUM_INODES_PER_PIECE];
+}
+
+struct inode {
+    int size;
+    int type;
+    int pointers[NUM_POINTERS_PER_INODE];
+}
